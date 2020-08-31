ca1e5a5404194b912095c08bb5c0190d177d6ae3
Juan Orlando <jcorlandomc@yahoo.com>
2020-08-30 23:42:35 -0700

    8/30/2020 11:43 pm

--- add_Fixed.srcs/sources_1/new/add_Fixed.v
+++ add_Fixed.srcs/sources_1/new/add_Fixed.v
@@ -1,9 +1,16 @@
 `timescale 1ns / 1ps
 
-module add_Fixed
+module add_Fixed # (parameter WI1 = 16, WF1 = 16, // input 1 integer and fraction bits
+                              WI2 = 16, WF2 = 16, // input 2 integer and fraction bits
+                              WIO = 16, WFO = 16) // output integer and fraction bits
 (
-    
+    input RESET,
+    input [WI1 + WF1 - 1 : 0] in1,
+    input [WI2 + WF2 - 1 : 0] in2,
+    output [WIO + WFO - 1 : 0] out
 );
+
+
 
 
 
