0250be961166ef82ae80cd3ae7890bdbda011c31
Juan Orlando <jcorlandomc@yahoo.com>
2020-09-01 13:16:54 -0700

    8/30/2020 11:43 pm

--- add_Fixed.srcs/sources_1/new/add_Fixed.v
+++ add_Fixed.srcs/sources_1/new/add_Fixed.v
@@ -1,18 +1,22 @@
 `timescale 1ns / 1ps
 
-module add_Fixed # (parameter WI1 = 16, WF1 = 16, // input 1 integer and fraction bits
-                              WI2 = 16, WF2 = 16, // input 2 integer and fraction bits
-                              WIO = 16, WFO = 16) // output integer and fraction bits
+module add_Fixed # (parameter WI1 = 4, WF1 = 4,   // input 1 integer and fraction bits
+                               WI2 = 4, WF2 = 4,   // input 2 integer and fraction bits
+         WIO = WI1 + WI2 - 1, WFO = WF1 + WF2 + 1) // output  integer and fraction bits
 (
     input RESET,
-    input [WI1 + WF1 - 1 : 0] in1,
-    input [WI2 + WF2 - 1 : 0] in2,
-    output [WIO + WFO - 1 : 0] out
+    input CLK,
+    input signed [WI1 + WF1 - 1 : 0] in1,     // Multiplicand
+    input signed [WI2 + WF2 - 1 : 0] in2,     // Multiplier
+    output reg signed [WIO + WFO - 1 : 0] out // Output
 );
-
-
-
-
-
+    
+    
+    always @ (*)
+    begin
+        if(RESET) out <= 0;
+        else out <= in1+in2;
+    end
+    
 endmodule
 
