64250d6da453dcdcede522d64761eb702e04f0a1
Juan Orlando <jcorlandomc@yahoo.com>
2020-09-07 01:21:11 -0700

    9/07/2020 01:20 pm

--- add_Fixed.srcs/sim_1/new/tb_add_Fixed.v
+++ add_Fixed.srcs/sim_1/new/tb_add_Fixed.v
@@ -2,43 +2,47 @@
 
 module tb_add_Fixed;
 // parameters
-parameter WI1 = 5, WF1 = 3,     // input 1 integer and fraction bits
-          WI2 = 3, WF2 = 6,     // input 2 integer and fraction bits
-          WIO = WI1 + WI2 - 1,  // output  integer and fraction bits
-          WFO = WF1 + WF2 + 1;  // output  integer and fraction bits
+parameter WI1 = 5, WF1 = 3,                     // input 1 integer and fraction bits
+          WI2 = 3, WF2 = 6,                     // input 2 integer and fraction bits
+          WIO = WI1 > WI2 ? WI1 + 1 : WI2 + 1,  // output integer bits
+          WFO = WF1 > WF2 ? WF1 : WF2;          // output fraction bits
 //Inputs
 reg RESET;
-reg CLK;
 reg signed [WI1 + WF1 - 1 : 0] in1;
 reg signed [WI2 + WF2 - 1 : 0] in2;
 //Outputs
 wire signed [WIO + WFO - 1 : 0] out;
-// Clock generation
-initial CLK <= 0;
-always #50 CLK <= ~CLK;
 //Instantiate DUT
 add_Fixed #( .WI1(WI1), .WF1(WF1), .WI2(WI2), .WF2(WF2), .WIO(WIO) )
-        DUT( .RESET(RESET), .CLK(CLK), .in1(in1), .in2(in2), .out(out) );
+        DUT( .RESET(RESET), .in1(in1), .in2(in2), .out(out) );
+        
+        wire [WI1 - 1 : 0] In1_int_bits = DUT.In1_int_bits;
+        wire [WF1 - 1 : 0] In1_frac_bits = DUT.In1_frac_bits;
+        wire [WI2 - 1 : 0] In2_int_bits = DUT.In2_int_bits;
+        wire [WF2 - 1 : 0] In2_frac_bits = DUT.In2_frac_bits;
+        
+        wire [WIO + WFO - 1 : 0] in1_Temp = DUT.in1_Temp;
+        wire [WIO + WFO - 1 : 0] in2_Temp = DUT.in2_Temp;
 
 initial
 begin
     RESET <= 0;
     in1 <= 8'b1_0110_110;
-    in2 <= 9'b0_01_110011;
-    @(posedge CLK);
-    @(posedge CLK);
+    in2 <= 9'b1_01_110011;
+    # 150;
+    # 150;
     in1 <= 8'b0_1011_111;
     in2 <= 9'b1_11_100110;
-    @(posedge CLK);
+    # 150;
     in1 <= 8'b0_0101_100;
     in2 <= 9'b0_10_100111;
-    @(posedge CLK);
+    # 150;
     RESET <= 1;
-    @(posedge CLK);
+    # 150;
     RESET <= 0;
     in1 <= 8'b1_1111_001;
     in2 <= 9'b1_01_000011;
-    @(posedge CLK);
+    # 150;
     in1 <= 8'b0_0101_100;
     in2 <= 9'b0_01_100111;
 end
--- add_Fixed.srcs/sources_1/new/add_Fixed.v
+++ add_Fixed.srcs/sources_1/new/add_Fixed.v
@@ -1,21 +1,47 @@
 `timescale 1ns / 1ps
 
-module add_Fixed # (parameter WI1 = 4, WF1 = 4,   // input 1 integer and fraction bits
-                               WI2 = 4, WF2 = 4,   // input 2 integer and fraction bits
-         WIO = WI1 + WI2 - 1, WFO = WF1 + WF2 + 1) // output  integer and fraction bits
+module add_Fixed # ( parameter  WI1 = 4, WF1 = 4,    // input 1 integer and fraction bits
+                                WI2 = 4, WF2 = 4,    // input 2 integer and fraction bits
+             WIO = WI1 > WI2 ? WI1 + 1 : WI2 + 1,    // output integer bits
+             WFO = WF1 > WF2 ? WF1 : WF2 )           // output fraction bits
 (
     input RESET,
-    input CLK,
-    input signed [WI1 + WF1 - 1 : 0] in1,     // Multiplicand
-    input signed [WI2 + WF2 - 1 : 0] in2,     // Multiplier
-    output reg signed [WIO + WFO - 1 : 0] out // Output
+    input signed [WI1 + WF1 - 1 : 0] in1,       // Add # 1
+    input signed [WI2 + WF2 - 1 : 0] in2,       // Add # 2
+    output reg signed [WIO + WFO - 1 : 0] out   // Output
 );
+    wire smallest_WI = WI1 < WI2 ? 1 : 0;    // 1 means WI1 is smaller; 0 means WI2 is smaller
+    wire smallest_WF = WF1 < WF2 ? 1 : 0;    // 1 means WF1 is smaller; 0 means WF2 is smaller
+    
+    wire [WI1 - 1 : 0] In1_int_bits  = in1[WI1 + WF1 - 1 : WF1];  // Just the integer  bits of in1
+    wire [WF1 - 1 : 0] In1_frac_bits = in1[WF1 - 1 : 0];          // Just the fraction bits of in1
+    wire [WI2 - 1 : 0] In2_int_bits  = in2[WI2 + WF2 - 1 : WF2];  // Just the integer  bits of in2
+    wire [WF2 - 1 : 0] In2_frac_bits = in2[WF2 - 1 : 0];          // Just the fraction bits of in2
+    
+    wire [WIO + WFO - 1 : 0] in1_Temp = { in1 , {((WIO + WFO)-(WI1 + WF1)){1'b0}} };
+    wire [WIO + WFO - 1 : 0] in2_Temp = { {((WIO + WFO)-(WI2 + WF2)){in2[WI2 + WF2 - 1]}} , in2 };
     
     
     always @ (*)
     begin
+        if(smallest_WI)
+        begin
+            //
+        end
+        else
+        begin
+            //
+        end
+        if(smallest_WF)
+        begin
+            //
+        end
+        else
+        begin
+            //
+        end
         if(RESET) out <= 0;
-        else out <= in1+in2;
+        else out <= in1 + in2;
     end
     
 endmodule
